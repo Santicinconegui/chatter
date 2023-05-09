291b18a7a4e9bfd0f97f9fa894d9cda1fe116ee3
santicinconegui <santiagocinconegui@gmail.com>
2023-05-09 13:47:30 -0300

    chatter

---
+++ frontend-desafio/src/components/Loading.tsx
@@ -0,0 +1,8 @@
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
+
+export const LoadStart = () => {
+  Loading.hourglass('Cargando...');
+};
+export const LoadRemove = () => {
+  Loading.remove();
+};
